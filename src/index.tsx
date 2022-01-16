5f125e6f77d4d8114ebb760b18a6d8903aaf6f93
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2022-01-16 13:52:35 +0700

    feat(graphql): create graphql provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,20 @@
 import { render } from 'react-dom';
 
 import Homepage from '@components/homepage';
+import { GraphqlProvider } from '@utils/graphql';
 
-render(<Homepage />, document.querySelector('#root'));
+/**
+ * Main Apps Component
+ *
+ * @author Irfan Andriansyah <irfan.andriansyah@tokopedia.com>
+ * @description main entrypoint apps component
+ * @returns {JSX.Element} react elementss
+ * @since 0.0.2
+ */
+const Apps = () => (
+  <GraphqlProvider>
+    <Homepage />
+  </GraphqlProvider>
+);
+
+render(<Apps />, document.querySelector('#root'));
